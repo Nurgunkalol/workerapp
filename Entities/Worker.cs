8da425221de90a24e29de4436548a193b9d61096
usb <nurgunka1991@gmail.com>
2017-10-28 01:17:22 +0900

    first commit

---
+++ Entities/Worker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace WorkersApp.Entities
+{
+    public class WorkersContext : DbContext
+    {
+        public DbSet<Worker> Workers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=workers.db");
+        }
+    }
+
+    public class Worker
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public Group Group { get; set; }
+        public DateTime EntryWorkDate { get; set; }
+        public double BaseRate { get; set; }
+    }
+
+    public enum Group
+    {
+        Employee,
+        Manager,
+        Salesman
+    }
+}
